3fabe452b71179d2c2bf7ffc2db62855e78f9705
leeedohyun <ldh19738@gmail.com>
2022-12-24 04:30:53 +0900

    Update 접미사 배열.py

--- 백준/Silver/11656. 접미사 배열/접미사 배열.py
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.py
@@ -1,3 +1,4 @@
+# 내가 푼 풀이
 from collections import deque
 
 S = input()
@@ -13,4 +14,15 @@ for i in range(len(S)):
 result.sort()
 
 for i in result:
+    print(i)
+  
+# 더 간결한 풀이
+S = input()
+
+suffix = []
+for i in range(len(S)) :
+    suffix.append(S[i:])
+
+suffix.sort()
+for i in suffix :
     print(i)
