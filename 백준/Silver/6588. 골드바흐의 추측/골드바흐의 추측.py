7fb7079a24044fb5e500c96527b91e14557b5e14
leeedohyun <ldh19738@gmail.com>
2022-12-26 17:44:48 +0900

    Update 골드바흐의 추측.py

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,3 +1,31 @@
+# 시간 초과 : number를 입력할 때마다 소수를 구해줌
+import math
+from sys import stdin
+
+while True:
+    number = int(stdin.readline())
+
+    if number == 0:
+        break
+
+    flag = [True for _ in range(1000000)]
+            
+    for i in range(2, int(math.sqrt(1000000)) + 1):
+        if flag[i]:
+            for j in range(i + i, 1000000, i):
+                flag[j] = False
+            
+    check = False
+    for i in range(3, number):
+        if flag[i] and flag[number - i]:
+                print("%d = %d + %d" % (number, i, number - i))
+                check = True
+                break
+
+    if not check:
+        print("Goldbach's conjecture is wrong.")
+
+# 입력할 수 있는 number의 범위 내의 소수를 한 번만 구하기
 import math
 from sys import stdin
 
