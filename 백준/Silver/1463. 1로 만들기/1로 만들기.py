980e5252727514807904d45ad97362d9cc830626
leeedohyun <ldh19738@gmail.com>
2022-12-30 17:04:29 +0900

    Update 1로 만들기.py

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,3 +1,31 @@
+'''
+처음 푼 풀이 : 입력 받는 X에서 1을 빼면 2또는 3으로 나누어 떨어진다는 사실을 생각하지 못하고
+            1을 빼는 과정을 while문으로 반복하여 시간초과 발생
+'''
+X = int(input())
+
+result = [0 for _ in range(1000001)]
+result[2] = 1
+
+for i in range(3, X + 1) :
+    n = i
+
+    while n != 1 :
+        a = n // 2
+        b = n // 3
+        if n % 3 == 0 and (result[b] < result[a] or b == 1) :
+            result[i] += result[b] + 1
+            break
+        elif n % 2 == 0 and result[a] < result[b] :
+            result[i] += result[a] + 1
+            break
+        else :
+            result[i] += 1
+            n -= 1
+    
+print(result[X])
+
+# 수정한 풀이
 X = int(input())
 
 result = [0 for _ in range(X + 1)]
