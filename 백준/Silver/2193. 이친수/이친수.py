2df16b0ee05e803b4e97455cd9850fc8c3de55e4
leeedohyun <ldh19738@gmail.com>
2023-01-04 08:31:16 +0900

    [Silver III] Title: 이친수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+dp = [[0 for _ in range(2)] for _ in range(91)]
+
+dp[1] = [0, 1]
+dp[2] = [1, 0]
+
+for i in range(3, 91) :
+    dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
+    dp[i][1] = dp[i - 1][1] + dp[i - 2][1]
+
+print(sum(dp[N]))
