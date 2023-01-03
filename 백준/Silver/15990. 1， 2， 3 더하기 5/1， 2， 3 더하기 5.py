c312b48f2c87bd48e4201e4dc0bc72736521e46a
leeedohyun <ldh19738@gmail.com>
2023-01-03 13:07:55 +0900

    Update 1， 2， 3 더하기 5.py

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -1,3 +1,4 @@
+# 내가 푼 풀이
 from sys import stdin
 
 input = stdin.readline
@@ -18,3 +19,21 @@ for i in range(T):
     n = int(input())
 
     print(sum(result[n]) % 1000000009)
+    
+# 메모리: 34272 KB, 시간: 136 ms
+import sys
+input = sys.stdin.readline
+
+mod = 1_000_000_009
+dp = [0,1,1,3] + [0]*99997
+a = [1, 0, 0]
+b = [0, 1, 0]
+c = [1, 1, 1]
+for i in range(4,100001):
+    temp = [(c[1]+c[2])%mod, (b[0]+b[2])%mod, (a[0]+a[1])%mod]
+    a, b, c = b, c, temp
+    dp[i] = sum(c)%mod
+
+test_case = int(input())
+for _ in range(test_case):
+    print(dp[int(input())])
