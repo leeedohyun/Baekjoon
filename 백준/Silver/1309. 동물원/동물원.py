d1a0bbf3a5de9bb0c67a0bf7c19544f376344f2f
leeedohyun <ldh19738@gmail.com>
2023-01-07 15:59:29 +0900

    Update 동물원.py

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,3 +1,21 @@
+# 내가 푼 풀이 : 이전의 행에 사자가 없는 경우, 왼쪽인 경우, 오른쪽인 경우로 나누어서 계산
+# 시간 : 820 ms
+from sys import stdin
+
+N = int(stdin.readline())
+
+dp = [1, 1, 1]
+
+for i in range(N - 1) :
+    a = dp[0] + dp[1] + dp[2]
+    b = dp[0] + dp[2]
+    c = dp[0] + dp[1]
+
+    dp = [a, b, c]
+
+print(sum(dp) % 9901)
+
+# 효율적인 풀이
 from sys import stdin
 
 N = int(stdin.readline())
