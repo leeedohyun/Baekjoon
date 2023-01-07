cfe34d969cb89790169ee1a1f96732804f5f4b4b
leeedohyun <ldh19738@gmail.com>
2023-01-07 15:37:45 +0900

    [Silver I] Title: 동물원, Time: 828 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+dp = [1, 1, 1]
+
+for i in range(N - 1) :
+    a = dp[0] + dp[1] + dp[2]
+    b = dp[0] + dp[2]
+    c = dp[0] + dp[1]
+
+    dp = [a, b, c]
+
+print(sum(dp) % 9901)
