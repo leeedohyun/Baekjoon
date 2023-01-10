273f48083be321e37b769a2308dc01b34eb32ea2
leeedohyun <ldh19738@gmail.com>
2023-01-10 14:46:44 +0900

    Update 가장 큰 증가 부분 수열.py

--- 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
@@ -1,3 +1,4 @@
+# 내가 푼 풀이
 import sys
 
 input = sys.stdin.readline
@@ -15,4 +16,12 @@ for i in range(1, n) :
         else :
             dp[i] = max(dp[i], A[i])
 
+print(max(dp))
+
+# 조금 더 효율적인 풀이 : 시간 (44 ms)
+input()
+dp = [0] * 1001
+for i in map(int, input().split()):
+    dp[i] = max(dp[:i]) + i         # 입력한 리스트의 요소들을 인덱스로 놓고 인덱스보다 작은 값 중 최댓값과 자기 자신을 더한 값을 저장
+    
 print(max(dp))
