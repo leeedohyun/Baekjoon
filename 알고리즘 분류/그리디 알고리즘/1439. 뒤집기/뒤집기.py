3155219e8ce1970331999c56dbe516b86071ac44
leeedohyun <ldh19738@gmail.com>
2023-02-07 11:54:43 +0900

    Create 뒤집기.py

---
+++ 알고리즘 분류/그리디 알고리즘/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,18 @@
+S = input()
+
+zero = 0
+one = 0
+
+if S[0] == '0' :
+    zero += 1
+else :
+    one += 1
+    
+for i in range(len(S) - 1) :
+    if S[i] != S[i + 1] :
+        if S[i + 1] == '0' :
+            zero += 1
+        else :
+            one += 1
+
+print(min(zero, one))
