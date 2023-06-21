0dd21a03e384016c21c8bef68b882ef83d959fa0
leeedohyun <ldh19738@gmail.com>
2023-06-21 14:20:39 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] numbers;
+    public static int[] arr;
+    public static int n, m;
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        numbers = new int[n];
+        visited = new boolean[n];
+        arr = new int[m];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solution(numbers, 0, 0);
+        System.out.println(sb);
+    }
+
+    public static void solution(int[] data, int start, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        Arrays.sort(data);
+
+        for (int i = start; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = data[i];
+                solution(data, i + 1, depth + 1);
+                visited[i] = false;
+            }
+        } 
+    }
+}
