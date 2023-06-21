4ac81ac8cadee586ecaa2fdf7dcc7c73577b07f0
leeedohyun <ldh19738@gmail.com>
2023-06-21 13:53:05 +0900

    [Silver III] Title: 이전 순열, Time: 384 ms, Memory: 18496 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static Integer[] numbers;
+    public static int n;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        numbers = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (solution(numbers)) {
+            for (int number : numbers) {
+                System.out.print(number + " ");
+            }
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    public static boolean solution(Integer[] data) {
+        int i = data.length - 1;
+        // data[i - 1] > data[i]
+        while (i > 0 && data[i - 1] <= data[i]) {
+            i -= 1;
+        }
+        if (i <= 0) {
+            return false;
+        }
+
+        int j = data.length - 1;
+        // date[i - 1] > data[j]
+        while (data[i - 1] <= data[j]) {
+            j -= 1;
+        }
+
+        int tmp = data[j];
+        data[j] = data[i - 1];
+        data[i - 1] = tmp;
+
+        Arrays.sort(data, i, data.length, Collections.reverseOrder());
+        return true;
+    }
+}
