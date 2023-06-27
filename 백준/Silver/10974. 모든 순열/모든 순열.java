2fade9196bea26abe1de0177457f2623ed220aef
leeedohyun <ldh19738@gmail.com>
2023-06-28 08:44:56 +0900

    [Silver III] Title: 모든 순열, Time: 268 ms, Memory: 22092 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,32 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     public static int n;
+    public static boolean[] visited; 
     public static int[] arr;
-    public static boolean[] visited;
     public static StringBuilder sb = new StringBuilder();
-
-    public static void main(String args[]) throws IOException {
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         n = Integer.parseInt(br.readLine());
-        arr = new int[n];
         visited = new boolean[n];
+        arr = new int[n];
         
         solution(0);
         System.out.println(sb);
-        
     }
-
+    
     public static void solution(int depth) {
         if (depth == n) {
             for (int i = 0; i < n; i++) {
                 sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
+            return;
         }
         
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 arr[depth] = i + 1;
