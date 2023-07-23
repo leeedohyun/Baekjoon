f3496389788b2763058a5213e89c94165f09c6a0
leeedohyun <ldh19738@gmail.com>
2023-07-23 21:28:38 +0900

    [Gold IV] Title: 알파벳, Time: 1040 ms, Memory: 15108 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,49 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int n, m;
-    public static int[][] board;
-    public static boolean[] visited = new boolean[26];
-    public static int max = 0;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static int r, c;
+	public static int[][] board;
+	public static boolean[] checked = new boolean[26];
+	public static int max = 0;
+	public static int[] dx = {0, 0, -1, 1};
+	public static int[] dy = {-1, 1, 0, 0};
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		
+		board = new int[r][c];
         
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
-        board = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < m; j++) {
-                board[i][j] = line.charAt(j) - 'A';
-            }
-        }
-        
-        visited[board[0][0]] = true;
-        dfs(0, 0, 1);
-        System.out.println(max);
-    }
-    
-    public static void dfs(int x, int y, int result) {
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-        
-        max = Math.max(max, result);
-        
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-            
-            if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m)
-                continue;
-           
-            if (!visited[board[nextX][nextY]]) {
-                visited[board[nextX][nextY]] = true;
-                dfs(nextX, nextY, result + 1);
-                visited[board[nextX][nextY]] = false;
-            }
-        }
-    }
+		for (int i = 0; i < r; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < c; j++) {
+				board[i][j] = line.charAt(j) - 'A';
+			}
+		}
+		dfs(0, 0, 0);
+		System.out.println(max);
+	}
+	
+	static void dfs(int x, int y, int cnt) {
+		if (checked[board[x][y]]) {
+			max = Math.max(max, cnt);
+			return;
+		}
+		else {
+			checked[board[x][y]] = true;
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+			
+				if(nx < 0 || ny < 0 || nx >= r || ny >= c) 
+                    continue;
+                
+				dfs(nx, ny, cnt + 1);
+			}
+			checked[board[x][y]] = false;
+		}
+	}
 }
