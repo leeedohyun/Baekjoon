4649d61cbfb9b5b3000239649e603d072e3d3cee
leeedohyun <ldh19738@gmail.com>
2023-08-29 10:59:06 +0900

    [Gold V] Title: 적록색약, Time: 164 ms, Memory: 17912 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -5,8 +5,6 @@ public class Main {
     public static int n, notBlindnessCount, blindnessCount;
     public static char[][] grid;
     public static boolean[][] visited;
-    public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, -1, 1};
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,7 +26,15 @@ public class Main {
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j]) {
                     notBlindnessCount += 1;
-                    findNotBlindness(i, j);
+                    dfs(i, j);
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 'G') {
+                    grid[i][j] = 'R';
                 }
             }
         }
@@ -36,12 +42,9 @@ public class Main {
         visited = new boolean[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 'G') {
-                    grid[i][j] = 'R';
-                }
                 if (!visited[i][j]) {
                     blindnessCount += 1;
-                    findBlindness(i, j);
+                    dfs(i, j);
                 }
             }
         }
@@ -52,7 +55,10 @@ public class Main {
         br.close();
     }
     
-    public static void findNotBlindness(int x, int y) {
+    public static void dfs(int x, int y) {
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        
         visited[x][y] = true;
         
         for (int i = 0; i < 4; i++) {
@@ -64,27 +70,7 @@ public class Main {
             
             if (!visited[nx][ny] && grid[x][y] == grid[nx][ny]) {
                 visited[nx][ny] = true;
-                findNotBlindness(nx, ny);
-            }
-        }
-    }
-    
-    public static void findBlindness(int x, int y) {
-        visited[x][y] = true;
-        
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) 
-                continue;
-            
-            if (grid[nx][ny] == 'G')
-                grid[nx][ny] = 'R';
-            
-            if (!visited[nx][ny] && grid[x][y] == grid[nx][ny]) {
-                visited[nx][ny] = true;
-                findBlindness(nx, ny);
+                dfs(nx, ny);
             }
         }
     }
